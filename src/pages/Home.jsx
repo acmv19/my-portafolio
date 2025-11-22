fb25af54d58bf211bc412837a057b7cc118a0a98
mava1 <anamariamaldonado1994@gmail.com>
2025-11-22 10:23:26 -0500

    adding dark mode

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+
+export const Home = () => {
+  return (
+    <div className=" bg-background text-foreground overflow-x-hidden">
+      {/*theme toggle*/}
+      <ThemeToggle />
+      {/*background Effects*/}
+
+      {/*nav bar*/}
+
+      {/*main content*/}
+
+      {/*footer*/}
+    </div>
+  );
+};
