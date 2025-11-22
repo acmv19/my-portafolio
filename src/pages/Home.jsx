3f6c4576df18e3bba397703965e0c3e12df76a97
mava1 <anamariamaldonado1994@gmail.com>
2025-11-22 10:55:43 -0500

    Theme Toggle and Background Effects, animation for star

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "../components/StarBackground";
 
 export const Home = () => {
   return (
@@ -6,6 +7,7 @@ export const Home = () => {
       {/*theme toggle*/}
       <ThemeToggle />
       {/*background Effects*/}
+      <StarBackground />
 
       {/*nav bar*/}
 
